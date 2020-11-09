dc6a72a9985cf59474fa315c9a3e20d353b6d49e
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-09 22:01:37 +0530

    [Kalyan] created binary search tree and added elements

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -6,6 +6,37 @@ namespace BinarySearchTree
 {
     public class BinarySearchTree
     {
-        
+        TreeNode root;
+        int treeSize = 0;
+        /// <summary>
+        /// First value will be set to root 
+        /// else the data will be added to right or left side of root 
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public void Insert(int data)
+        {
+            if (root == null)
+            {
+                root = new TreeNode(data);
+                treeSize++;
+            }
+            else
+            {
+                root.Insert(data);
+                treeSize++;
+            }
+        }
+        public void Display()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Binary Search Tree is Empty");
+            }
+            else
+            {
+                root.InOrderTraversal();
+            }
+        }
+
     }
 }

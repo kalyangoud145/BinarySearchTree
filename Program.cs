8f268c95574e1b969b890afb8a262024a8f08ec2
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-09 21:52:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to binary search tree");
+        }
+    }
+}
